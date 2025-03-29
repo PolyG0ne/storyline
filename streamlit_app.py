2c5b64e0c3fca22606c5765bf3684741b3618c18
PolyG0ne <witty.rcj@gmail.com>
2025-03-29 17:25:27 -0400

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
-import random
-import pandas as pd
-import altair as alt
+
 
 # Configuration de la page
 st.set_page_config(page_title="Timeline Verticale", layout="wide")
@@ -9,9 +7,11 @@ st.set_page_config(page_title="Timeline Verticale", layout="wide")
 # Titre de l'application
 st.title("Timeline de ma progression professionnelle")
 
+
+st.title("1980")
 st.markdown(
     """
-    1980\n
+    |\n
     |\n
     |\n
     |\n
